fb3307a67095f6f943bdbbb2ad4d84a500cd02b7
Cecilia <cema3961@colorado.edu>
2020-01-15 11:29:32 -0800

    PIL input

--- detectron2/engine/defaults.py
+++ detectron2/engine/defaults.py
@@ -181,10 +181,9 @@ class DefaultPredictor:
             if self.input_format == "RGB":
                 # whether the model expects BGR inputs or RGB
                 original_image = original_image[:, :, ::-1]
-            height, width = original_image.shape[:2]
+            height, width = original_image.size[:2]
             image = self.transform_gen.get_transform(original_image).apply_image(original_image)
-            image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
-
+            
             inputs = {"image": image, "height": height, "width": width}
             predictions = self.model([inputs])[0]
             return predictions
