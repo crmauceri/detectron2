6c333d80939be8bb80ffd4011edc9da3c4feb69f
Cecilia <cecilia.mauceri@colorado.edu>
2020-01-23 18:25:44 -0700

    Tested convertion. One-to-one assignment. Next step is to test loading and running network

--- tools/convert_deeplab_net.py
+++ tools/convert_deeplab_net.py
@@ -4,7 +4,7 @@ import re
 
 def convert_resnet(filepath):
     backbone = {k:v for k, v in torch.load(filepath)['state_dict'].items() if k.startswith('backbone')}
-    p = re.compile('backbone\.layer([0-9])\.([0-9]).\w+([1-3]).(\w+)')
+    p = re.compile('backbone\.layer([0-9])\.([0-9]+)\.(\w+)([\.0-3]+)\.(\w+)')
 
     new_backbone = {}
     for k, v in backbone.items():
@@ -19,36 +19,38 @@ def convert_resnet(filepath):
         elif k == "backbone.bn1.running_var":
             new_backbone['res_conv1_bn_running_var']= v
             continue
-        elif k == "backbone.bn1.weights":
+        elif k == "backbone.bn1.weight":
             new_backbone['res_conv1_bn_gamma']= v
             continue
         elif k == "backbone.bn1.bias":
             new_backbone['res_conv1_bn_beta']= v
             continue
+        elif k == "backbone.bn1.num_batches_tracked":
+            continue
 
         #Rules start here
         match = re.match(p, k)
         if match:
-            index1 = match.group(0)
-            index2 = match.group(1)
-            layer = match.group(2)
-            branch = match.group(3)
-            variable = match.group(4)
+            index1 = match.group(1)
+            index2 = match.group(2)
+            layer = match.group(3)
+            branch = match.group(4)
+            variable = match.group(5)
 
             if layer == 'downsample':
                 b = '2c'
-            elif branch == 1:
-                b = 1
-            elif branch == 2:
+            elif branch == '1':
+                b = '1'
+            elif branch == '2':
                 b = '2a'
-            elif branch == 2:
+            elif branch == '3':
                 b = '2b'
             else:
                 raise ValueError(k)
 
             if layer == 'conv':
                 variable = 'w'
-            elif layer == 'downsample' and branch == 0:
+            elif layer == 'downsample' and branch == '.0':
                 variable = 'w'
             elif layer in ['bn', 'downsample'] and variable == 'weight':
                 variable = 'gamma'
@@ -57,7 +59,7 @@ def convert_resnet(filepath):
             elif variable == 'num_batches_tracked':
                 continue
 
-            new_k = 'res_{}_{}_branch{}_{}'.format(index1+1, index2, b, variable)
+            new_k = 'res_{}_{}_branch{}_{}'.format(int(index1)+1, index2, b, variable)
 
             new_backbone[new_k] = v
         else:
